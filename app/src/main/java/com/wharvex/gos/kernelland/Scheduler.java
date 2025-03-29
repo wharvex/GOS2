a3e2b249400835a1f75d30bca8036f9432e0e47d
Tim <81175852+wharvex@users.noreply.github.com>
2025-03-29 19:16:57 -0400

    scheduler prep

--- app/src/main/java/com/wharvex/gos/kernelland/Scheduler.java
+++ app/src/main/java/com/wharvex/gos/kernelland/Scheduler.java
@@ -1,4 +1,30 @@
 package com.wharvex.gos.kernelland;
 
+import com.wharvex.gos.osland.ProcessWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+
 public class Scheduler {
+  private final Timer timer;
+  private final List<ProcessWrapper> processes;
+  private static volatile ProcessWrapper currentProcess;
+
+  public Scheduler() {
+    timer = new Timer("timerThread");
+    processes = new ArrayList<>();
+  }
+
+  public static ProcessWrapper getCurrentProcess() {
+    return currentProcess;
+  }
+
+  public static void setCurrentProcess(ProcessWrapper process) {
+    currentProcess = process;
+  }
+
+  public void startTimer() {
+    timer.schedule(new TimerTaskExt(), 0, 1000);
+  }
 }
---
+++ app/src/main/java/com/wharvex/gos/kernelland/TimerTaskExt.java
@@ -0,0 +1,10 @@
+package com.wharvex.gos.kernelland;
+
+import java.util.TimerTask;
+
+public class TimerTaskExt extends TimerTask {
+  @Override
+  public void run() {
+
+  }
+}
